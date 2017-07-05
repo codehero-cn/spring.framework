c07da6aac337b6514605dde9264e4472154bc57d
eleven <eleven@ling>
2017-07-06 06:26:07 +0800

    Signed-off-by: eleven <eleven@ling>

--- src/main/java/cn/codehero/spring/beans/cycle/Main.java
+++ src/main/java/cn/codehero/spring/beans/cycle/Main.java
@@ -1,25 +1,22 @@
 package cn.codehero.spring.beans.cycle;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * 测试主程序.
- * 
  * @author eleven
  */
 public class Main {
 
 	public static void main(String[] args) {
 		
-		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("/src/main/java/cn/codehero/spring/beans/cycle/applicationContext.xml");
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("/cn/codehero/spring/beans/cycle/applicationContext.xml");
 		
-		Car car = (Car) ctx.getBean("car");
+		Car car = (Car) ac.getBean("car");
 		System.out.println(car);
 		
 		//关闭IOC
-		ctx.close();
+		ac.close();
 	}
 
 }
